e59fd1fbd3a205bed8fa5103c476b1abfc8a60b9
AnatolyRybchych <tol.ryb@gmail.com>
2022-07-26 12:59:30 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace AudioSampler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("qwe");
+        }
+    }
+}
